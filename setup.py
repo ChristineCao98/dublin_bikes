58b7b1ab277eee4db367a438232f822ea23913aa
AnYiHuang <anyi.579nm@gmail.com>
2021-03-01 01:39:56 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="dublin-bike",
+      version="0.1",
+      description="Dublin Bike Availability Prediction",
+      url="",
+      author="AY",
+      author_email="",
+      licence="GPL3",
+      packages=['scraper'],
+      entry_points={
+        'console_scripts':['bike_scraper=scraper.scheduler:main']
+        }
+      )
