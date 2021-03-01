b82020a9d441e0909f7096a3af245d4a12583558
AnYiHuang <anyi.579nm@gmail.com>
2021-03-01 18:56:59 +0000

    add a basic and runnable routes.py

---
+++ flask_app/routes.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, jsonify
+from models.schemas import DublinBike
+from flask_sqlalchemy import SQLAlchemy
+from flask_caching import Cache
+from sqlalchemy import func
+from config.config import MySQL
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = MySQL.URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CACHE_TYPE'] = 'simple'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+
+db = SQLAlchemy(app)
+cache = Cache(app)
+
+
+@app.route('/api/stations/')
+@cache.cached()
+def get_all_stations():
+    """Return details of all stations"""
+    latest_scraping_time = db.session \
+        .query(func.max(DublinBike.scraping_time)) \
+        .one()[0]
+
+    stations = db.session.query(DublinBike) \
+        .filter(DublinBike.scraping_time == latest_scraping_time) \
+        .order_by(DublinBike.number.asc()) \
+        .all()
+
+    return jsonify({
+        'data': [station.serialize for station in stations]
+    })
+
+
+@app.route('/api/stations/<int:station_id>')
+@cache.cached()
+def get_station(station_id):
+    """Return details of the station"""
+    station = db.session.query(DublinBike) \
+        .filter(DublinBike.number == station_id) \
+        .order_by(DublinBike.scraping_time.desc()) \
+        .first()
+
+    return jsonify({
+        'data': station.serialize
+    })
+
+
+if __name__ == '__main__':
+    app.run()
