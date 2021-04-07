ca1255e09a7bd392dd4f64d2b3a125919c1a7ffa
AnYiHuang <anyi.huang@ucdconnect.ie>
2021-04-07 15:30:15 +0100

    logging config

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -17,7 +17,7 @@ from scraper.weather_forecast_scraper import scrape
 import logging
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG,
-                    format=f'%(asctime)s %(levelname)s %(name)s : %(message)s')
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 
 app = Flask(__name__)
